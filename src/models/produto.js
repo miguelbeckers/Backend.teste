957ba8f0c0454f9f18999a5f68220bb9b66f6e54
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2020-11-23 18:40:28 -0300

    mudanca nos models

--- src/models/produto.js
+++ src/models/produto.js
@@ -4,15 +4,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    nome: {
+    descricao: {
         type: String,
         required: true,
         trim: true
     },
-    dataNascimento: {
-        type: String,
+    preco: {
+        type: Number,
         required: true,
-        trim: true
     }
 });
 
