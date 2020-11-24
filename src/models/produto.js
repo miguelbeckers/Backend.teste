ac9deb49df08588ac0b9de8116370ad8bdc46cc0
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2020-11-24 14:34:10 -0300

    Revert "mudanca nos models"
    This reverts commit 957ba8f0c0454f9f18999a5f68220bb9b66f6e54.

--- src/models/produto.js
+++ src/models/produto.js
@@ -4,14 +4,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    descricao: {
+    nome: {
         type: String,
         required: true,
         trim: true
     },
-    preco: {
-        type: Number,
+    dataNascimento: {
+        type: String,
         required: true,
+        trim: true
     }
 });
 
